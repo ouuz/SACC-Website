49c6f4184841919ffd490cbaf8d19399f09c625c
HKFPouu <389746410@qq.com>
2019-10-04 22:09:10 +0800

    feat: 组别展示 & 主席团展示完成，（差填充的具体数据）

--- js/index.js
+++ js/index.js
@@ -1,38 +1,62 @@
-// LogoPage
-// GroupPage
-// TeamPage
-// ContactPage
-let LogoPageAngel = 0,
-    GroupPageAngel = 0,
-    TeamPageAngel = 0,
-    ContactPageAngel = 0;
-$(document).keydown(function (e) {
+const teamImgToText = () => {
+  let list = document.querySelector('.teamRight');
+  list.addEventListener('mouseover', slideIn);
+  list.addEventListener('mouseout', slideOut);
 
-
-  switch (e.which) {
-    case 39: //右
-      LogoPageAngel += 90;
-      GroupPageAngel += 90;
-      TeamPageAngel += 90;
-      ContactPageAngel += 90;
-      $('.LogoPage').css('transform', `rotate(${LogoPageAngel}deg)`)
-      $('.GroupPage').css('transform', `rotate(${GroupPageAngel}deg)`)
-      $('.GroupPage').css('height', `50vh`)
-      $('.TeamPage').css('transform', `rotate(${TeamPageAngel}deg)`)
-      $('.ContactPage').css('transform', `rotate(${ContactPageAngel}deg)`)
-      break;
-    case 37: //左
-      LogoPageAngel -= 90;
-      GroupPageAngel -= 90;
-      TeamPageAngel -= 90;
-      ContactPageAngel -= 90;
-      $('.LogoPage').css('transform', `rotate(${LogoPageAngel}deg)`)
-      $('.GroupPage').css('transform', `rotate(${GroupPageAngel}deg) scale(2.1)`)
-      $('.GroupPage').css('height', `100vh`)
-      $('.TeamPage').css('transform', `rotate(${TeamPageAngel}deg)`)
-      $('.ContactPage').css('transform', `rotate(${ContactPageAngel}deg)`)
-      break;
-
+  function slideIn(e) {
+    e.target.classList.contains('groupImgBox') ? slide() : '';
+    function slide() {
+      $($(e.target).children().get(0)).css('transform', 'translateX(-200%)')
+      $($(e.target).children().get(1)).css('transform', 'translateX(-200%)')
+      $(e.target).next().css('opacity', '1')
+      $('.teamLeft').empty().append(`
+      <img src=${$($(e.target).children().get(0)).attr('src')} alt=""  class="teamLeftLogo">
+      `)
+    }
   }
 
+  function slideOut(e) {
+    e.target.classList.contains('groupText') ? slide() : '';
+
+    function slide() {
+      setTimeout(() => {
+        $($(e.target).prev().children().get(0)).css('transform', 'translateX(0)')
+        $($(e.target).prev().children().get(1)).css('transform', 'translateX(0)')
+        $(e.target).css('opacity', '0')
+      }, 100)
+    }
+  }
+}
+
+
+const presidiumDrag = () => {
+  $('.presidiumBottom').bind("selectstart",function(){return false;});  
+
+  let list = document.querySelector('.presidiumBottom'),
+    then, now;
+  list.addEventListener('mousedown', dragDown);
+  list.addEventListener('mouseup', dragUp);
+
+  function dragDown(e) {
+    then = e.offsetX;
+  }
+
+  function dragUp(e){
+    now = e.offsetX;
+    (now - then < 50) && (now - then < 0)?
+    $('.presidiumBottom').css('transform','translateX(-50%)')
+    :
+    $('.presidiumBottom').css('transform','translateX(0)')
+  }
+}
+
+$('.competionName').mouseover(()=>{
+    $('.competionName').next().css('animation','competionIn 1s 1 forwards')
 })
+
+$('.competionName').mouseout(()=>{
+    $('.competionName').next().css('animation','competionOut 1s 1 forwards')
+})
+
+teamImgToText()
+presidiumDrag()
